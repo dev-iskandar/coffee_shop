b7dbbae4747133a96b5a500ef2b73ecd0dd7f27b
Iskandar <iskandarbegimkulov43@email.com>
2026-04-19 20:47:52 +0500

    fix image field to char

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     slug = models.SlugField(unique=True, max_length=150)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_name = models.CharField(max_length=150)
+    image_name = models.CharField(max_length=150, default='no-image.jpg')
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
