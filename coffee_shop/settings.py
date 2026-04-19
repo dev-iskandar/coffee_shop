fa8d725c37ccab432a50dbd3651ccd0ab2793e69
Iskandar <iskandarbegimkulov43@email.com>
2026-04-19 19:42:08 +0500

    fix jpg

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
